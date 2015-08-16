2b5f5566f6b7161cf968e2fd9b29ca2775863561
Hidetaka <hidetaka.48691001@icloud.com>
2015-08-17 04:32:20 +0900

    Finish user signup

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,12 +1,17 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
-
+  
+  resources :users
   root  'static_pages#home'
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
-  get "static_pages/contact"
-  end
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+  match 'signin', to: 'static_pages#signin', via: 'get'
+   match '/signup',  to: 'users#new',            via: 'get'
+  
+  
+  
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -62,3 +67,4 @@ SampleApp::Application.routes.draw do
   #     resources :products
   #   end
 
+end
